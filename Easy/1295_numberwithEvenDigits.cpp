de054cf6766be7d3ab977be84b7e46295d803da5
Bharath200415 <bharath200415@gmail.com>
2025-07-27 00:17:06 +0530

    update

---
+++ Easy/1295_numberwithEvenDigits.cpp
@@ -0,0 +1,30 @@
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> fizzBuzz(int n){
+
+}
+
+int main(){
+    int arr[] = {437,315,322,431,534};
+    int even = 0;
+    for (int ele:arr){
+        int digits=0;
+
+        while (ele>0){
+            ele/=10;
+            digits++;
+        }
+        if (digits%2==0){
+            even+1;
+        }else{
+            continue;
+        }
+
+    }
+    cout<<even;
+
+
+}
