7940ebaff9f93e73790de2e26c8e084c7d2241be
Bharath200415 <bharath200415@gmail.com>
2025-08-24 00:51:19 +0530

    updates

---
+++ Medium/MajorityEle.java
@@ -0,0 +1,41 @@
+import java.util.*;
+public class MajorityEle{
+    static void checkEle(int arr[]){
+        int n = arr.length;
+        int maxCount=0;
+        int index=-1;
+        for (int i=0; i<n;i++){
+            int count =0;
+            for (int j =0;j<n;j++){
+                if (arr[i]==arr[j]){
+                    count++;
+                }
+            }
+            if (count>maxCount){
+                maxCount=count;
+                index = i;
+            }
+
+        }
+        if (maxCount>n/2){
+                System.out.println(arr[index]);
+                
+
+            }else{
+                System.out.println("No majority elements");
+            }
+        
+
+        
+
+
+    }
+    public static void main(String args[]){
+        int[] arr = {1,1,2,2,2,2,2};
+        checkEle(arr);
+
+
+    }
+    
+
+}
