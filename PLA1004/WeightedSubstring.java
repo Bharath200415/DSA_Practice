198d2a175b1d4e34788e7064e9337a88e6f9c44e
Bharath200415 <bharath200415@gmail.com>
2025-09-25 11:20:06 +0530

    weighted substring

---
+++ PLA1004/WeightedSubstring.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class WeightedSubstring{
+    static int distinctsub(String P, String Q, int K, int N){
+
+        HashSet<String> S = new HashSet<String>();
+        
+        for (int i=0;i<N;i++){
+            int sum = 0;
+
+            String s = "";
+            //abcde 
+            for (int j=i;j<N;j++){
+                int pos = P.charAt(j)-'a';
+                sum+= Q.charAt(pos)-'0';
+                s+= P.charAt(j);
+
+                if (sum<=K){
+                    S.add(s);
+                }else{
+                    break;
+                }
+            }
+        }
+
+        return S.size();
+
+    }
+    public static void main(String[] args) {
+        String P = "abcde";
+        String Q = "12345678912345678912345678";
+        int K = 5;
+        int N = P.length();
+
+        System.out.print(distinctsub(P, Q, K, N));
+    }
+
+}
