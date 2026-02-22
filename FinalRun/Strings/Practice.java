402df96c882e5290123bde1657ac4f1a0d3dd30c
Bharath200415 <bharath200415@gmail.com>
2026-02-22 18:06:27 +0530

    updated

---
+++ FinalRun/Strings/Practice.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Practice {
+    public static void main(String[] args) {
+        int res = 555;
+        
+        String ans =res+"helo";
+        System.out.println(ans);
+    }
+    
+}
